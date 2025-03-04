f78a12e0430483611b59485b1bc38b292a672dc4
arsanyBenyamine <arsany.benyamine@yomicepa.com>
2025-03-04 07:34:18 +0200

    fix Header

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,15 @@
 import Header from "@/components/Headers/Header/Header"
+import { getProfile } from "../actions/auth"
 
-export default function AuthLayout({
+export default async function AuthLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+    const user = await getProfile()
     return (
         <div>
-            <Header isLoggedIn={false} />
+            <Header user={user} />
             {children}
         </div>
     )
